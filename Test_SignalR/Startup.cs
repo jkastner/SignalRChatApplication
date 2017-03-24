cb6ad0e281d95a522b25678809b5f50e841d48de
JohnathanK <johnathan.kastner@gmail.com>
2017-03-24 16:18:35 -0600

    A pile of scripts -- probably better ways to store these.

---
+++ Test_SignalR/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Test_SignalR.Startup))]
+namespace Test_SignalR
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
